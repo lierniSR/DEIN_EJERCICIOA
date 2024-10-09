d670f36837905e554686a6012c3237ff3d575370
Lierni <lierni@gmail.com>
2024-10-09 09:44:45 +0200

    Metodo del boton aceptar la parte de la alerta de informacion echo

--- src/main/java/es/liernisarraoa/encuesta/HelloApplication.java
+++ src/main/java/es/liernisarraoa/encuesta/HelloApplication.java
@@ -4,21 +4,25 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
+import javafx.stage.Window;
 
 public class HelloApplication extends Application {
+    private static Scene scene;
 
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("encuesta.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 625, 580);
-        stage.setTitle("Hello!");
+        scene = new Scene(fxmlLoader.load(), 625, 580);
+        stage.setTitle("Encuesta!");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
         launch();
+    }
+
+    public static Scene getScene(){
+        return scene;
     }
 }
--- src/main/java/es/liernisarraoa/encuesta/controladores/HelloController.java
+++ src/main/java/es/liernisarraoa/encuesta/controladores/HelloController.java
@@ -1,12 +1,15 @@
 package es.liernisarraoa.encuesta.controladores;
 
 
+import es.liernisarraoa.encuesta.HelloApplication;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
+
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -15,6 +18,8 @@ import static java.lang.System.exit;
 
 
 public class HelloController implements Initializable {
+    private Scene sceneApplication;
+
     @FXML
     private TextField labelProfesion;
 
@@ -61,15 +66,47 @@ public class HelloController implements Initializable {
     private VBox VBoxPantalla;
     
     public void visualizarLista(ActionEvent actionEvent) {
-        String[] listaString = new String[] {"Futbol", "Tenis", "Baloncesto"};
-        if(checkBoxPractica.isSelected()){
-            listaDeportes.setDisable(false);
-        } else {
-            listaDeportes.setDisable(true);
-        }
+        listaDeportes.setDisable(!checkBoxPractica.isSelected());
     }
 
     public void mensajeInfo(ActionEvent actionEvent) {
+        sceneApplication = HelloApplication.getScene();
+        alerta();
+    }
+
+    public void alerta(){
+        ObservableList<String> deportesSeleccionados = listaDeportes.getSelectionModel().getSelectedItems();
+        String deportes = "";
+        for(int i = 0; i < deportesSeleccionados.size(); i++){
+            deportes += "\t" + deportesSeleccionados.get(i) + "\n";
+        }
+        SingleSelectionModel<String> edadSeleccionada = comboBoxEdad.getSelectionModel();
+        String sexoSeleccionado = ((RadioButton) RadioGrupo.getSelectedToggle()).getText();
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.initOwner(sceneApplication.getWindow());
+        alert.setHeaderText(null);
+        alert.setTitle("TUS DATOS");
+        if(checkBoxPractica.isSelected()){
+            alert.setContentText("Profesion: " + labelProfesion.getText() + "\n" +
+                    "Nº de Hermanos: " + labelHermanos.getText() + "\n" +
+                    "Edad: " + edadSeleccionada.getSelectedItem() + "\n" +
+                    "Sexo: " + sexoSeleccionado + "\n" +
+                    "Deportes que practicas: \n" +
+                    deportes +
+                    "Grado de aficcion a las compras: " + sliderCompras.getValue() + "\n" +
+                    "Grado de aficcion a ver la television: " + sliderTelevision.getValue() + "\n" +
+                    "Grado de aficcion a ir al cine: " + sliderCine.getValue() + "\n");
+        } else {
+            alert.setContentText("Profesion: " + labelProfesion.getText() + "\n" +
+                    "Nº de Hermanos: " + labelHermanos.getText() + "\n" +
+                    "Edad: " + edadSeleccionada.getSelectedItem() + "\n" +
+                    "Sexo: " + sexoSeleccionado + "\n" +
+                    "Grado de aficcion a las compras: " + sliderCompras.getValue() + "\n" +
+                    "Grado de aficcion a ver la television: " + sliderTelevision.getValue() + "\n" +
+                    "Grado de aficcion a ir al cine: " + sliderCine.getValue() + "\n");
+        }
+
+        alert.showAndWait();
     }
 
     public void salirPrograma(ActionEvent actionEvent) {
